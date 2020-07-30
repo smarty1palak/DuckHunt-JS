496fe3bfd0d4cf23b34922f97f40b8656a063153
Palak Singhal <palak.singhal1@myntra.com>
2020-07-30 13:59:31 +0530

    spelling mistake

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/data',(req, res) => {
   var user=req.body.user;
-  var xcord=req.body.xcord;
-  var ycord=req.body.ycord;
+  var xcord=req.body.xcoord;
+  var ycord=req.body.ycoord;
   var hitmiss=req.body.hitmiss;
   var time=req.body.time;
   console.log(user,xcord,ycord,hitmiss,time);
